01897048169b3eed3d84d47ccf1fc8b6d82abb1f
Administrator <admin@local.host>
2019-04-28 08:34:01 +0200

    clear 2

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/donky/labcode_gatsby/src/layouts/index.js"
-  import data from "/home/donky/labcode_gatsby/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
